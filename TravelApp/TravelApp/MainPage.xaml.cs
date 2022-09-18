5ad37a982f82b9a4353111ce83bad7c0bb4d57e1
Talish George <talishtest@gmail.com>
2022-09-18 22:28:26 +1000

    Added login page fields

--- TravelApp/TravelApp/MainPage.xaml.cs
+++ TravelApp/TravelApp/MainPage.xaml.cs
@@ -14,6 +14,20 @@ namespace TravelApp
         {
             InitializeComponent();
         }
+
+        void loginButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
+            if (isEmailEmpty || isEmailEmpty)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
     }
 }
 
